1dca95091d42881b5ae11b8f751f8add5e72e5fd
Trent Hoeffel <trenthoeffel@gmail.com>
2025-04-11 00:03:57 -0400

    Reapply "fixed multiple clients crashing"
    This reverts commit 98b5c9cf300e29fd3320624bd4e9f67504fd57fa.

--- Client.java
+++ Client.java
@@ -38,30 +38,24 @@ public class Client extends JFrame {
         add(new JScrollPane(jta), BorderLayout.CENTER);
 
         jb.addActionListener(new ButtonListener()); // Register listener
+<<<<<<< HEAD
         jtfPhoneNumber.addActionListener(new ButtonListener()); // Register listener
+=======
+>>>>>>> parent of 98b5c9c (Revert "fixed multiple clients crashing")
 
         setTitle("Client");
         setSize(500, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true); // It is necessary to show the frame here!
 
-        try {
-            // Create a socket to connect to the server
-            Socket socket = new Socket("localhost", 8000);
-
-            // Create an input stream to receive data from the server
-            fromServer = new DataInputStream(socket.getInputStream());
-
-            // Create an output stream to send data to the server
-            toServer = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException ex) {
-            jta.append(ex.toString() + '\n');
-        }
     }
 
     private class ButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            try {
+            try (Socket socket = new Socket("localhost", 8000);
+                 DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+                 DataInputStream fromServer = new DataInputStream(socket.getInputStream())) {
+
                 // Get the name and phone number from the text fields
                 String name = jtfName.getText().trim();
                 String phoneNumber = jtfPhoneNumber.getText().trim();
@@ -70,16 +64,18 @@ public class Client extends JFrame {
                 toServer.writeUTF(name);
                 toServer.writeUTF(phoneNumber);
                 toServer.flush();
-
                 // Get the server response
                 String serverResponse = fromServer.readUTF();
 
                 // Display to the text area
                 jta.append("Sent contact: " + name + " - " + phoneNumber + "\n");
                 jta.append("Server response: " + serverResponse + "\n");
+                
+                socket.close();
             } catch (IOException ex) {
                 jta.append(ex.toString() + '\n');
             }
         }
     }
+
 }
